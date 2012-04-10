a37d64c033ccb9e27661c0864024eb5c73ef4c2d
Spencer Ward <spencertward@gmail.com>
2012-04-10 13:19:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spencerward::Application.config.secret_token = 'c12b963623acf33bc2146fe79bc129574ba377b2a1f8d6404f2e7ac9c170c019c9a5480a88d7b90509660e5b2ceb3bdbfdf78a1c6f68b57296b810a2a7b0101c'
